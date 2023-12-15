cdf30e6177c210bd22cc8d282397ec8e80dc70f1
Kaveesha Gimhana <36946467+KaveeshaG@users.noreply.github.com>
2023-12-16 04:40:18 +0530

    fix: changed localhost

--- src/components/menuCategories/MenuCategories.jsx
+++ src/components/menuCategories/MenuCategories.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import styles from "./menuCategories.module.css";
 
 const getData = async () => {
-  const res = await fetch("http://localhost:3000/api/categories", {
+  const res = await fetch("http://127.0.0.1:3000/api/categories", {
     cache: "no-store",
   });
 
